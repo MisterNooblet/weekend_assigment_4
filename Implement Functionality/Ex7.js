2ab28ef9dd97d4ca64cd3d39d1817b34d0f2e76d
misternooblet <dragonroadsupport@gmail.com>
2023-01-21 14:09:24 +0200

    Ex7

--- Implement Functionality/Ex7.js
+++ Implement Functionality/Ex7.js
@@ -5,3 +5,27 @@
 // - Map
 // Using only for(), array and objects (without otherjsmethods)
 
+///Filter///
+
+let filterArr = [1,76,555,32,75,97]
+let filtered = []
+for(let i = 0; i < filterArr.length; i++){
+    if (filterArr[i] > 77) filtered.push(filterArr[i])
+}
+
+///ForEach///
+let forEaArr = ['2',2,'ooooo',2,7,8]
+for(let i = 0; i < forEaArr.length; i++){
+    console.log(i);
+}
+///Map///
+let mapArr= [ 6,5,4,32,2,65,3456,3245235,1241]
+let mapped = []
+for(let i = 0; i < mapArr.length; i++){
+    mapped.push(mapArr[i]*2)
+}
+
+
+
+
+
