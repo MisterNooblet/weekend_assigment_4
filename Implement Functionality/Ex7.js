a13914344390afd2dd72a56be0380669cc3ef741
misternooblet <dragonroadsupport@gmail.com>
2023-01-21 14:23:54 +0200

    Done

--- Implement Functionality/Ex7.js
+++ Implement Functionality/Ex7.js
@@ -5,27 +5,47 @@
 // - Map
 // Using only for(), array and objects (without otherjsmethods)
 
-///Filter///
-
+///Filter///Array
 let filterArr = [1,76,555,32,75,97]
 let filtered = []
 for(let i = 0; i < filterArr.length; i++){
     if (filterArr[i] > 77) filtered.push(filterArr[i])
 }
 
-///ForEach///
+///ForEach///Array
 let forEaArr = ['2',2,'ooooo',2,7,8]
 for(let i = 0; i < forEaArr.length; i++){
     console.log(i);
 }
-///Map///
+///Map///Array
 let mapArr= [ 6,5,4,32,2,65,3456,3245235,1241]
 let mapped = []
 for(let i = 0; i < mapArr.length; i++){
     mapped.push(mapArr[i]*2)
 }
 
+/////////////////////////////////////
+///Filter///Object
+let obj = {
+    0: 77,
+    1: 99,
+    2: 2323,
+    3: 324,
+    4: 95,
+}
+    let objFiltered={}
 
+for(let i = 0; i < Object.keys(obj).length; i++){
+    if (obj[i] > 77) objFiltered[i] = obj[i]
+}
 
+///forEach///Object
+for(let i = 0; i < Object.keys(obj).length; i++){
+   console.log(obj[i]);
+}
 
-
+///Map///Object
+let mappedObj = {}
+for(let i = 0; i < Object.keys(obj).length; i++){
+    mappedObj[i] = (obj[i]*2)
+}
